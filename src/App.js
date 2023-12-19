be739f19125bf565f466d849a0c4b16c40457772
Ayisha-SS <13aiysh246@gmail.com>
2023-12-19 09:30:57 +0530

    build for deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Todo from './Todoapp/Todo';
 
 function App(){
+   
   return (
     <div className="App">
       <Todo/>
+     
     </div>
   )
 }
